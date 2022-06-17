d658fb04f6d41a03d8fa62ed510e6965be44b581
alantheandroid <alessio.ilardi@gmail.com>
2022-06-17 17:57:23 +0200

    feat: add 'stringify' JSON method

--- exercise-1.js
+++ exercise-1.js
@@ -7,5 +7,6 @@ class Person {
   }
 }
 
-const developer = new Person(1, 'Mario', 'Rossi', 25);
+const developer = new Person(1, "Mario", "Rossi", 25);
 // Print developer as json object
+console.log(JSON.stringify(developer));
